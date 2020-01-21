fb312139d9ec44fa72beaccfabcf653fd73f800f
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-01-21 19:48:05 +0900

    v1.6_InterFreq_Codes
    Add High to Low codes

--- Inter Frequency/Idle_InterFreq_LB.py
+++ Inter Frequency/Idle_InterFreq_LB.py
@@ -51,7 +51,7 @@ decide = pd.Series([])
 result = pd.Series([])
 
 
-#Equal Priority
+# Equal Priority
 if B1st_to_B2nd == Equal:
 
     sNonIntraSearchP = param_dict['SIB1_q-RxLevMin'][0] + \
@@ -99,7 +99,7 @@ if B1st_to_B2nd == Equal:
     # Plot scatter
     data_B1st = data_B1st.sort_values('Result')
     ax = sns.scatterplot(x=f'{B2nd}_RSRP (dBm)', y=f'{B1st}_RSRP (dBm)',
-                         hue='Result', data=data_B1st, palette=['steelblue','goldenrod','seagreen'])
+                         hue='Result', data=data_B1st, palette=['steelblue', 'goldenrod', 'seagreen'])
 
     # Define Straight Line Function
     x = np.linspace(pmin, pmax)
@@ -112,7 +112,7 @@ if B1st_to_B2nd == Equal:
     searchline = sNonIntraSearchP
     y = searchline
     plt.axhline(y, color='b', linestyle='--')
-    plt.text(pmin, y + 1, f'SIB3_s-NonIntraSearchP = {searchline} dBm')
+    plt.text(pmin, y + 1, f'SIB1_q-RxLevMin + SIB3_s-NonIntraSearchP = {searchline} dBm')
 
     # Plot Decision line
     decideline = param_dict['SIB5_q-OffsetFreq'][0] + \
@@ -132,8 +132,8 @@ if B1st_to_B2nd == Equal:
     plt.show()
 
 
-#Low to High
-elif B1st_to_B2nd == LowtoHigh
+# Low to High
+elif B1st_to_B2nd == LowtoHigh:
 
     # Create Search Criteria
     index = 0
@@ -171,7 +171,7 @@ elif B1st_to_B2nd == LowtoHigh
     # Plot scatter
     data_B1st = data_B1st.sort_values('Result')
     ax = sns.scatterplot(x=f'{B2nd}_RSRP (dBm)', y=f'{B1st}_RSRP (dBm)',
-                         hue='Result', data=data_B1st, palette=['goldenrod','seagreen'])
+                         hue='Result', data=data_B1st, palette=['goldenrod', 'seagreen'])
 
     # Define Straight Line Function
     x = np.linspace(pmin, pmax)
@@ -181,10 +181,12 @@ elif B1st_to_B2nd == LowtoHigh
     plt.plot(x, y, color='k', linestyle='-')
 
     # Plot Decision line
-    decideline = param_dict['SIB5_q-RxLevMin'][0] + param_dict['SIB5_threshX-HighP'][0]
+    decideline = param_dict['SIB5_q-RxLevMin'][0] + \
+        param_dict['SIB5_threshX-HighP'][0]
     y = decideline
     plt.axvline(y, color='b', linestyle='--')
-    plt.text(pmin, y + 1, f'SIB5_q-RxLevMin + SIB5_threshX-HighP = {decideline} dBm')
+    plt.text(
+        y + 1, pmin, f'SIB5_q-RxLevMin + SIB5_threshX-HighP = {decideline} dBm')
 
     plt.xlabel(f'{B2nd}_RSRP (dBm)')
     plt.ylabel(f'{B1st}_RSRP (dBm)')
@@ -196,12 +198,91 @@ elif B1st_to_B2nd == LowtoHigh
     plt.show()
 
 
-########################################################Currently testing#######################################################
-# REVIEW:
+# High to Low Priority
+if B1st_to_B2nd == HightoLow:
+
+    sNonIntraSearchP = param_dict['SIB1_q-RxLevMin'][0] + \
+        param_dict['SIB3_s-NonIntraSearchP'][0]
+
+    # Create Search Criteria
+    index = 0
+    for index in range(len(data_B1st)):
+
+        if data_B1st[f'{B1st}_RSRP (dBm)'][index] < sNonIntraSearchP:
+            search[index] = 1
+        else:
+            search[index] = 0
+
+    data_B1st['Search'] = search
+
+    # Create Decision Criteria
+    index = 0
+    for index in range(len(data_B1st)):
+
+        if ((data_B1st[f'{B1st}_RSRP (dBm)'][index] < (param_dict['SIB1_q-RxLevMin'][0] + param_dict['SIB3_threshServingLowP'][0])) and (data_B1st[f'{B2nd}_RSRP (dBm)'][index] > (param_dict['SIB5_q-RxLevMin'][0] + param_dict['SIB5_threshX-LowP'][0]))):
+            decide[index] = 1
+        else:
+            decide[index] = 0
+
+    data_B1st['Decision'] = decide
+
+    # Result
+    index = 0
+    for index in range(len(data_B1st)):
+
+        if data_B1st['Search'][index] + data_B1st['Decision'][index] == 2:
+            result[index] = f'03 Move to {B2nd}'
+        elif data_B1st['Search'][index] == 1:
+            result[index] = '02 Search Only'
+        else:
+            result[index] = '01 Not Searching'
+
+        data_B1st['Result'] = result
+
+    # Plot definition
+    pmax = -70
+    pmin = -140
+
+    # Plot scatter
+    data_B1st = data_B1st.sort_values('Result')
+    ax = sns.scatterplot(x=f'{B2nd}_RSRP (dBm)', y=f'{B1st}_RSRP (dBm)',
+                         hue='Result', data=data_B1st, palette=['steelblue', 'goldenrod', 'seagreen'])
+
+    # Define Straight Line Function
+    x = np.linspace(pmin, pmax)
+    y = x
+
+    # Plot Equal Line
+    plt.plot(x, y, color='k', linestyle='-')
+
+    # Plot Search line
+    searchline = sNonIntraSearchP
+    y = searchline
+    plt.axhline(y, color='b', linestyle='--')
+    plt.text(pmin, y + 1, f'SIB1_q-RxLevMin + SIB3_s-NonIntraSearchP = {searchline} dBm')
+
+    #Plot Decision line
+    threshServingLowP = param_dict['SIB1_q-RxLevMin'][0] + param_dict['SIB3_threshServingLowP'][0]
+    decideline = threshServingLowP
+    y = decideline
+    plt.axhline(y, color='b', linestyle='--')
+    plt.text(pmin, y + 1, f'SIB1_q-RxLevMin + SIB3_threshServingLowP = {decideline} dBm')
+
+    threshXLowP = param_dict['SIB5_q-RxLevMin'][0] + param_dict['SIB5_threshX-LowP'][0]
+    decideline = threshXLowP
+    y = decideline
+    plt.axvline(y, color='b', linestyle='--')
+    plt.text(y + 1, pmin, f'SIB5_q-RxLevMin + SIB5_threshX-LowP = {decideline} dBm')
 
 
-########################################################Currently testing#######################################################
+    plt.xlabel(f'{B2nd}_RSRP (dBm)')
+    plt.ylabel(f'{B1st}_RSRP (dBm)')
+    plt.title(f'[RSRP] {B1st} Reselection to {B2nd} [High -> Low Priority]')
+    plt.grid(color='gray', linestyle='--', linewidth=0.5)
+    plt.ylim((pmin, pmax))
+    plt.xlim((pmin, pmax))
 
+    plt.show()
 
 print(' ')
 print('ありがとうございました！！')
